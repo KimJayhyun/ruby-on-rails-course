79d27c3df58e6294f1424112f2daedc1b8b27434
KimJayhyun <dnlwjem000@gmail.com>
2026-03-15 16:51:53 +0900

    feat: create article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,19 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+
+    if @article.save      # 성공하면 true, 실패하면 false
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render :new, status: :unprocessable_entity # 실패하면 폼으로 돌아가기
+    end
+  end
 end
